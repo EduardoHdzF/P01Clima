d62d3e3bbe73e15e735dc9419070d50da052edff
JavierMQ28 <javier_manceraq@ciencias.unam.mx>
2022-09-12 20:22:35 -0500

    Muestra los climas de las ciudades

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import imp
-from operator import pos
-
+#import imp
+#from operator import pos
+from asyncore import read
+import json
+from urllib.request import urlopen
 
 print("--- CLIMA ---")
 
@@ -68,7 +70,7 @@ iata_codes = {
         "Latitud de origen" : lista_coordenadas[int(indice_prop) - 1][8:15],
         "Longitud de origen" : lista_coordenadas[int(indice_prop) - 1][16:23],
         "Latitud de destino" : lista_coordenadas[int(indice_prop) - 1][24:31],
-        "Longitud de destino" : lista_coordenadas[int(indice_prop) - 1][32:39]
+        "Longitud de destino" : lista_coordenadas[int(indice_prop) - 1][32:39],        
     }
 
 }
@@ -76,7 +78,43 @@ iata_codes = {
 # Imprimir la ciudad seleccionada
 print(iata_codes.get(lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]))
 # print(iata_codes.get(input()))
+
+# Obtener las coordenadas de la ciudad de origen
+long_org = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Longitud de origen']
+lat_org = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Latitud de origen']
+
+# Obtener las coordenadas de la ciudad de destino
+long_des = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Longitud de destino']
+lat_des = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Latitud de destino']
 #'''
+
+# Llamada Api de ciudad de origen
+url_modif_org = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_org) + "&lon=" + str(long_org) + "&appid=b2844c1e815b5b3dde610589df05cad2"
+url_org = url_modif_org
+
+with urlopen(url_org) as json_dicc_org:
+    json_data_org = json_dicc_org.read()
+
+# Imprime el clima de la ciudad de origen (diccionario).
+clima_org = json.loads(json_data_org)
+print(json.dumps(clima_org,indent = 2))
+
+# Llamada Api de ciudad de destino
+url_modif_des = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_des) + "&lon=" + str(long_des) + "&appid=b2844c1e815b5b3dde610589df05cad2"
+url_des = url_modif_des
+
+with urlopen(url_des) as json_dicc_des:
+    json_data_des = json_dicc_des.read()
+
+# Imprime el clima de la ciudad de origen (diccionario).
+clima_des = json.loads(json_data_des)
+print(json.dumps(clima_des,indent = 2))
+
+# Clima de la ciudad de origen:
+print("- Clima de la ciudad de origen -\n    Condición actual : " + clima_org ['weather'][0]['main'] + "\n    Descripción : " + clima_org ['weather'][0]['description'] + "\n    Temperatura : " , clima_org ['main']['temp'] , "\n    Temperatura mínima : " , clima_org ['main']['temp_min'] , "\n    Temperatura máxima : " , clima_org ['main']['temp_max'] , "\n    Humedad (%) : " , clima_org ['main']['humidity'] , "\n    Velocidad del viento : " , clima_org ['wind']['speed'] , "\n    Nubes : " , clima_org ['clouds']['all'] , "\n\n")
+
+# Clima de la ciudad de destino:
+print("- Clima de la ciudad de destino -\n    Condición actual : " + clima_des ['weather'][0]['main'] + "\n    Descripción : " + clima_des ['weather'][0]['description'] + "\n    Temperatura : " , clima_des ['main']['temp'] , "\n    Temperatura mínima : " , clima_des ['main']['temp_min'] , "\n    Temperatura máxima : " , clima_des ['main']['temp_max'] , "\n    Humedad (%) : " , clima_des ['main']['humidity'] , "\n    Velocidad del viento : " , clima_des ['wind']['speed'] , "\n    Nubes : " , clima_des ['clouds']['all'])
 
 ## Creacion de un diccionario con una ciudad en particular.
 '''
