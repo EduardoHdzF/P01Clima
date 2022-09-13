41a2428d8fa054faea1318fd445a04c958cf5e6c
Your Name <eduardohf@ciencias.unam.mx>
2022-09-13 03:34:31 -0500

    Primeras pruebas con la BD

---
+++ ClimaVuelos/Programa/Entrada/datosEntrada.py
@@ -0,0 +1,4 @@
+'''Para que pueda ejecutarse correctamente el programa, debemos ejecutarlo desde la
+    carpeta P01Clima
+'''
+ArchivoEntrada = open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+')
