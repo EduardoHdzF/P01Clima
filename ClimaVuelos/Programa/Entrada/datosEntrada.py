cc9a7f1d7f60ce2884089f892ebcd53e12ee33e4
Your Name <eduardohf@ciencias.unam.mx>
2022-09-13 17:07:52 -0500

    Ya se entiende como leer e imprimir el archivo

--- ClimaVuelos/Programa/Entrada/datosEntrada.py
+++ ClimaVuelos/Programa/Entrada/datosEntrada.py
@@ -1,4 +1,15 @@
 '''Para que pueda ejecutarse correctamente el programa, debemos ejecutarlo desde la
     carpeta P01Clima
 '''
-ArchivoEntrada = open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+')
+
+with open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+') as ArchivoEntrada:
+
+#ArchivoEntrada = open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+')
+    print(type(ArchivoEntrada))
+
+    print(ArchivoEntrada.readline())
+
+    for linea in ArchivoEntrada:
+        print(linea)
+
+    print(ArchivoEntrada.readlines())
