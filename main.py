39a0c96a9dd62ae789701f037c03aeec377e1111
JavierMQ28 <javier_manceraq@ciencias.unam.mx>
2022-09-14 19:57:43 -0500

    Quita comentarios innecesarios

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ while terminado == 1:
     ciudades_archivo_csv = open("dataset1.csv","r")
 
     historial = open("lista.txt","r")
-    #actuliza_dicc = open("lista.txt","w+")
     actuliza_dicc = open("lista.txt","a+")
 
     lista_coordenadas = []    
@@ -54,9 +53,6 @@ while terminado == 1:
             print(str(posicion) + ".- " , lista_ciudades[viaje][ciudad] , " -> " , lista_ciudades[viaje][ciudad + 1])
             posicion += 1
     
-    # Creacion de diccionario.
-    #'''
-
     # Para manejar las excepciones.
     def manejo_errores(canti_ciudades):
         
@@ -92,10 +88,6 @@ while terminado == 1:
     iata_codes = {
 
         lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7] : {
-            #"Latitud de origen" : lista_coordenadas[int(indice_prop) - 1][8:15],
-            #"Longitud de origen" : lista_coordenadas[int(indice_prop) - 1][16:23],
-            #"Latitud de destino" : lista_coordenadas[int(indice_prop) - 1][24:31],
-            #"Longitud de destino" : lista_coordenadas[int(indice_prop) - 1][32:39]
             "Latitud de origen" : lista_coordenadas[int(indice_prop) - 1][lista_coordenadas[int(indice_prop) - 1].index(coord_lat_origen) + coord_lat_origen.index(',') + 1:lista_coordenadas[int(indice_prop) - 1].index(coord_long_origen)],
             "Longitud de origen" : lista_coordenadas[int(indice_prop) - 1][lista_coordenadas[int(indice_prop) - 1].index(coord_long_origen) + coord_long_origen.index(',') + 1:lista_coordenadas[int(indice_prop) - 1].index(coord_lat_destino) + 1],
             "Latitud de destino" : lista_coordenadas[int(indice_prop) - 1][lista_coordenadas[int(indice_prop) - 1].index(coord_lat_destino) + coord_lat_destino.index(',') + 1:-11],
@@ -111,8 +103,7 @@ while terminado == 1:
     # Obtener las coordenadas de la ciudad de destino
     long_des = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Longitud de destino']
     lat_des = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Latitud de destino']
-    #'''
-
+    
     ciudad_org_cache = False
 
     ciudad_des_cache = False
@@ -124,16 +115,6 @@ while terminado == 1:
             }
         }
     else:
-        '''
-        diccionario_guardar = {}
-
-        with open("lista.txt") as lista:
-            
-            for linea in lista:
-
-                (llave, valor) = linea.split()
-                diccionario_guardar[int(llave)] = valor
-        '''        
         diccionario_guardar = {}
 
         historial.seek(0)
@@ -154,31 +135,6 @@ while terminado == 1:
 
             ciudad_des_cache = True             
 
-    """
-    # Llamada Api de ciudad de origen
-    url_modif_org = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_org) + "&lon=" + str(long_org) + "&appid=b2844c1e815b5b3dde610589df05cad2"
-    url_org = url_modif_org
-
-    with urlopen(url_org) as json_dicc_org:
-        json_data_org = json_dicc_org.read()
-
-    # Imprime el clima de la ciudad de origen (diccionario).
-    clima_org = json.loads(json_data_org)
-    print(json.dumps(clima_org,indent = 2))
-
-    # Llamada Api de ciudad de destino
-    url_modif_des = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_des) + "&lon=" + str(long_des) + "&appid=b2844c1e815b5b3dde610589df05cad2"
-    url_des = url_modif_des
-
-    print(lat_org + " --- " + long_org)
-    print(lat_des + " --- " + long_des)
-    with urlopen(url_des) as json_dicc_des:
-        json_data_des = json_dicc_des.read()
-
-    # Imprime el clima de la ciudad de origen (diccionario).
-    clima_des = json.loads(json_data_des)
-    print(json.dumps(clima_des,indent = 2))
-    """
     if not ciudad_org_cache and not ciudad_des_cache:
 
         # Llamada Api de ciudad de origen
@@ -266,91 +222,10 @@ while terminado == 1:
 
         # Clima de la ciudad de destino:
         print("-- Clima de la ciudad de origen -\n    Condición actual : " + clima_des ['Clima'] + "\n    Descripción : " + clima_des ['Descripcion'] + "\n    Temperatura : " , clima_des ['Temperatura'] , "\n    Temperatura mínima : " , clima_des ['Temperatura minima'] , "\n    Temperatura máxima : " , clima_des ['Temperatura maxima'] , "\n    Humedad (%) : " , clima_des ['Humedad'] , "\n    Velocidad del viento : " , clima_des ['Velocidad del viento'] , "\n    Nubes : " , clima_des ['Nubes'] , "\n    Nombre : " , clima_des ['Nombre'])
-    
-    # Agrega los datos visualizados por ultima vez al diccionario que se guardara en un archivo.
-    """
-    if len(historial.readlines()) == 0:
-        diccionario_guardar = { 
-            "Ciudad" : {
-                
-            }
-        }
-    else:
-        '''
-        diccionario_guardar = {}
-
-        with open("lista.txt") as lista:
-            
-            for linea in lista:
-
-                (llave, valor) = linea.split()
-                diccionario_guardar[int(llave)] = valor
-        '''        
-        diccionario_guardar = {}
-
-        historial.seek(0)
-        diccionario_guardar = eval(historial.readline())        
-        print("Diccionario = " , diccionario_guardar)
-    """    
-
-    """
-    "- Clima de la ciudad de destino -\n    
-    Condición actual : " + clima_des ['weather'][0]['main'] + "\n    
-    Descripción : " + clima_des ['weather'][0]['description'] + "\n    
-    Temperatura : " , clima_des ['main']['temp'] , "\n    
-    Temperatura mínima : " , clima_des ['main']['temp_min'] , "\n    
-    Temperatura máxima : " , clima_des ['main']['temp_max'] , "\n    
-    Humedad (%) : " , clima_des ['main']['humidity'] , "\n    
-    Velocidad del viento : " , clima_des ['wind']['speed'] , "\n    
-    Nubes : " , clima_des ['clouds']['all'] , "\n    
-    Nombre : " , clima_des ['name']
-    """
-    
+        
     actuliza_dicc.truncate(0)
     
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Nombre"] = clima_org ['name']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Clima"] = clima_org['weather'][0]['main']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Descripcion"] = clima_org ['weather'][0]['description']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura"] = clima_org ['main']['temp']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura minima"] = clima_org ['main']['temp_min']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura maxima"] = clima_org ['main']['temp_max']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Humedad"] = clima_org ['main']['humidity']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Velocidad del viento"] = clima_org ['wind']['speed']
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Nubes"] = clima_org ['clouds']['all']
-    
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Clima"].append(clima_des['weather'][0]['main'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Descripcion"].append(clima_des ['weather'][0]['description'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura"].append(clima_des ['main']['temp'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura minima"].append(clima_des ['main']['temp_min'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura maxima"].append(clima_des ['main']['temp_max'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Humedad"].append(clima_des ['main']['humidity'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Velocidad del viento"].append(clima_des ['wind']['speed'])
-    #diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Nubes"].append(clima_des ['clouds']['all'])
-
-    '''
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Nombre"] = clima_des ['name']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Clima"] = clima_des['weather'][0]['main']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Descripcion"] = clima_des ['weather'][0]['description']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura"] = clima_des ['main']['temp']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura minima"] = clima_des ['main']['temp_min']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Temperatura maxima"] = clima_des ['main']['temp_max']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Humedad"] = clima_des ['main']['humidity']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Velocidad del viento"] = clima_des ['wind']['speed']
-    diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]]["Nubes"] = clima_des ['clouds']['all']
-    '''            
-
-    #historial.write(str(diccionario_guardar))
-    actuliza_dicc.write(str(diccionario_guardar))
-    #actuliza_dicc.write(json.dumps(diccionario_guardar, indent=2))
-    #actuliza_dicc.write(str(diccionario_guardar.keys()))
-    #actuliza_dicc.write(str(diccionario_guardar.values()))
-    #actuliza_dicc.write(str(diccionario_guardar.items()))
-    #for clave, valor in diccionario_guardar.items():
-    #    guardado = (clave , valor)
-
-    #actuliza_dicc.write(str(diccionario_guardar))
-
-    print(len(historial.readlines()))
+    actuliza_dicc.write(str(diccionario_guardar))    
 
     def finaliza():
         
@@ -378,89 +253,6 @@ while terminado == 1:
 
     # El usuario decide si se finaliza el programa o no.
     terminado = finaliza()
-
-    ## Creacion de un diccionario con una ciudad en particular.
-    '''
-    iata_codes_ejemplo = {
-
-        lista_ciudades[0][0:3] and lista_ciudades[0][4:7] : {
-            "Latitud de origen" : lista_ciudades[0][8:15],
-            "Longitud de origen" : lista_ciudades[0][16:23],
-            "Latitud de destino" : lista_ciudades[0][24:31],
-            "Longitud de destino" : lista_ciudades[0][32:39]
-            }
-
-    }
-
-    print(lista_ciudades[0][0:3])
-    print(iata_codes_ejemplo.get(lista_ciudades[0][0:3] and lista_ciudades[0][4:7]))
-    print(iata_codes_ejemplo.get("ACA" and "MEX"))
-    print(len(iata_codes_ejemplo))
-    '''
-
-    ## Imprimir el tamanio de la lista
-    '''
-    print(len(lista_ciudades))
-    '''
-
-    ## Prueba de comparaciones hace caso omiso del segundo que se esta comparando cuando se utilizan dos readlines.
-    ## Imprime el siguiente si se utiliza solo una vez.
-    '''
-    lista_ciudades.append(ciudades_archivo_csv.readline())
-
-    #if ciudades_archivo_csv.readline() != ciudades_archivo_csv.readline:
-    #    print(ciudades_archivo_csv.readline())
-
-    if lista_ciudades[0] != ciudades_archivo_csv.readline:
-        print(ciudades_archivo_csv.readline())
-    '''
-
-    ## Reiniciar leer las lineas del archivo.
-    '''
-    ciudades_archivo_csv.seek(0)
-    '''
-
-    ## Aniadir una ciudad en particular a mi lista.
-    '''
-    ciudad = ciudades_archivo_csv.readlines()[1][0:3]
-    print(ciudad[0])
-    #print(ciudad[1])
-    lista_ciudades.append(ciudad)
-    print(lista_ciudades)
-    '''
-
-    ## Comparar dos ciudades una en la lista y otra en el archivo
-    ### - Iguales
-    '''
-    ciudades_archivo_csv.readline()
-    ciudades_archivo_csv.readline()
-    ciudades_archivo_csv.readline()
-    ciudades_archivo_csv.readline()
-    ciudades_archivo_csv.readline()
-
-
-    ### - No iguales
-    """
-    ciudades_archivo_csv.readline()
-    """
-
-    ciudad1 = ciudades_archivo_csv.readline()
-    print(ciudad1)
-
-    ciudad2 = ciudades_archivo_csv.readline()
-    print(ciudad2)
-
-    lista_ciudades.append(ciudad1)
-    print(lista_ciudades)
-
-    if lista_ciudades[0] == ciudad2:
-        print("-- Son iguales.")
-    else:
-        print("-- No son iguales.")
-
-    if lista_ciudades[0] == ciudad2:
-        print(ciudad2)
-    '''
 
     ciudades_archivo_csv.close()
 
