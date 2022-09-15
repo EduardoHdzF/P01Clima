c04a01ebcb41ac61999a0cdb0b836e40c0988880
JavierMQ28 <javier_manceraq@ciencias.unam.mx>
2022-09-14 19:52:28 -0500

    Quita prints que no sirven

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ while terminado == 1:
 
     print("--- CLIMA ---")
 
-    print("Seleccione las ciudades de origen y destino donde se quiere saber su clima de cada uno:")
+    print("Seleccione el viaje que desea realizar para saber el clima de cada ciudad (origen y destino):")
 
     ciudades_archivo_csv = open("dataset1.csv","r")
 
@@ -16,11 +16,7 @@ while terminado == 1:
     #actuliza_dicc = open("lista.txt","w+")
     actuliza_dicc = open("lista.txt","a+")
 
-    print("- Ciudad de origen -")
-
-    print("- Ciudad de destino -")
-
-    lista_coordenadas = []
+    lista_coordenadas = []    
 
     # Agregar las ciudades a mi lista (sin repeticiones).
     for iter in range(len(ciudades_archivo_csv.readlines())):
@@ -38,13 +34,9 @@ while terminado == 1:
                 lista_coordenadas.append(ciudades_archivo_csv.readlines()[iter])                                
                 ciudades_archivo_csv.seek(0)          
 
-    print(lista_coordenadas)
-    print("** Tamanio de la lista: " , len(lista_coordenadas) , "\n")
-
     # Ordenamos lista.
-    lista_coordenadas.sort()
-    print(lista_coordenadas)
-
+    lista_coordenadas.sort()    
+    
     # Creacion de una lista para el usuario para que pueda ver cual opcion escoger.
     lista_ciudades = []
     posicion = 1
@@ -53,9 +45,7 @@ while terminado == 1:
         
         ciudades_org_des = [lista_coordenadas[iter][0:3] , lista_coordenadas[iter][4:7]]
         lista_ciudades.append(ciudades_org_des)
-
-    print(lista_ciudades)
-
+        
     # Despliega la lista de viajes disponibles.
     for viaje in range(len(lista_ciudades)):
             
@@ -63,7 +53,7 @@ while terminado == 1:
 
             print(str(posicion) + ".- " , lista_ciudades[viaje][ciudad] , " -> " , lista_ciudades[viaje][ciudad + 1])
             posicion += 1
-
+    
     # Creacion de diccionario.
     #'''
 
@@ -76,7 +66,7 @@ while terminado == 1:
 
             try:
 
-                ciudad_selec = int(input("Escoge un viaje:"))
+                ciudad_selec = int(input("Escoge un viaje (revisa la lista):\n"))
 
                 if ciudad_selec > 0 and ciudad_selec <= canti_ciudades:
 
@@ -92,14 +82,12 @@ while terminado == 1:
 
         return ciudad_selec
 
-    indice_prop = manejo_errores(len(lista_ciudades))
-    print(indice_prop)
+    indice_prop = manejo_errores(len(lista_ciudades))    
 
     coord_lat_origen = lista_coordenadas[int(indice_prop) - 1][6:]
     coord_long_origen = lista_coordenadas[int(indice_prop) - 1][14:]
     coord_lat_destino = lista_coordenadas[int(indice_prop) - 1][22:]
-    coord_long_destino = lista_coordenadas[int(indice_prop) - 1][30:]
-    print(coord_lat_destino + " *** " + coord_long_destino)
+    coord_long_destino = lista_coordenadas[int(indice_prop) - 1][30:]    
 
     iata_codes = {
 
@@ -114,11 +102,7 @@ while terminado == 1:
             "Longitud de destino" : lista_coordenadas[int(indice_prop) - 1][lista_coordenadas[int(indice_prop) - 1].index(coord_long_destino) + coord_long_destino.index(',') + 1:-1]
         }
 
-    }
-
-    # Imprimir la ciudad seleccionada
-    print(iata_codes.get(lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]))
-    # print(iata_codes.get(input()))
+    }        
 
     # Obtener las coordenadas de la ciudad de origen
     long_org = iata_codes [lista_coordenadas[int(indice_prop) - 1][0:3] and lista_coordenadas[int(indice_prop) - 1][4:7]] ['Longitud de origen']
@@ -153,8 +137,7 @@ while terminado == 1:
         diccionario_guardar = {}
 
         historial.seek(0)
-        diccionario_guardar = eval(historial.readline())        
-        print("Diccionario = " , diccionario_guardar)
+        diccionario_guardar = eval(historial.readline())               
 
         ciudad1 = diccionario_guardar["Ciudad"].get(lista_coordenadas[int(indice_prop) - 1][0:3])
 
@@ -169,8 +152,7 @@ while terminado == 1:
             ciudad_org_cache = True
         elif ciudad1 == None and ciudad2 != None:
 
-            ciudad_des_cache = True
-         
+            ciudad_des_cache = True             
 
     """
     # Llamada Api de ciudad de origen
@@ -202,37 +184,33 @@ while terminado == 1:
         # Llamada Api de ciudad de origen
         url_modif_org = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_org) + "&lon=" + str(long_org) + "&appid=b2844c1e815b5b3dde610589df05cad2"
         url_org = url_modif_org
-
+        
         with urlopen(url_org) as json_dicc_org:
             json_data_org = json_dicc_org.read()
 
         # Imprime el clima de la ciudad de origen (diccionario).
-        clima_org = json.loads(json_data_org)
-        print(json.dumps(clima_org,indent = 2))
-
+        clima_org = json.loads(json_data_org)        
+        
         # Llamada Api de ciudad de destino
         url_modif_des = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat_des) + "&lon=" + str(long_des) + "&appid=b2844c1e815b5b3dde610589df05cad2"
         url_des = url_modif_des
 
-        print(lat_org + " --- " + long_org)
-        print(lat_des + " --- " + long_des)
         with urlopen(url_des) as json_dicc_des:
             json_data_des = json_dicc_des.read()
 
         # Imprime el clima de la ciudad de origen (diccionario).
-        clima_des = json.loads(json_data_des)
-        print(json.dumps(clima_des,indent = 2))
+        clima_des = json.loads(json_data_des)        
 
         # Clima de la ciudad de origen:
         print("- Clima de la ciudad de origen -\n    Condición actual : " + clima_org ['weather'][0]['main'] + "\n    Descripción : " + clima_org ['weather'][0]['description'] + "\n    Temperatura : " , clima_org ['main']['temp'] , "\n    Temperatura mínima : " , clima_org ['main']['temp_min'] , "\n    Temperatura máxima : " , clima_org ['main']['temp_max'] , "\n    Humedad (%) : " , clima_org ['main']['humidity'] , "\n    Velocidad del viento : " , clima_org ['wind']['speed'] , "\n    Nubes : " , clima_org ['clouds']['all'] , "\n    Nombre : " , clima_org ['name'] , "\n\n")
 
         # Clima de la ciudad de destino:
         print("- Clima de la ciudad de destino -\n    Condición actual : " + clima_des ['weather'][0]['main'] + "\n    Descripción : " + clima_des ['weather'][0]['description'] + "\n    Temperatura : " , clima_des ['main']['temp'] , "\n    Temperatura mínima : " , clima_des ['main']['temp_min'] , "\n    Temperatura máxima : " , clima_des ['main']['temp_max'] , "\n    Humedad (%) : " , clima_des ['main']['humidity'] , "\n    Velocidad del viento : " , clima_des ['wind']['speed'] , "\n    Nubes : " , clima_des ['clouds']['all'] , "\n    Nombre : " , clima_des ['name'])
-
+        
         diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][0:3]] = { "Nombre" : clima_org ['name'] , "Clima" : clima_org['weather'][0]['main'] , "Descripcion" : clima_org ['weather'][0]['description'] , "Temperatura" : clima_org ['main']['temp'] , "Temperatura minima" : clima_org ['main']['temp_min'] , "Temperatura maxima" : clima_org ['main']['temp_max'] , "Humedad" : clima_org ['main']['humidity'] , "Velocidad del viento" : clima_org ['wind']['speed'] , "Nubes" : clima_org ['clouds']['all']}
 
         diccionario_guardar["Ciudad"][lista_coordenadas[int(indice_prop) - 1][4:7]] = { "Nombre" : clima_des ['name'] , "Clima" : clima_des['weather'][0]['main'] , "Descripcion" : clima_des ['weather'][0]['description'] , "Temperatura" : clima_des ['main']['temp'] , "Temperatura minima" : clima_des ['main']['temp_min'] , "Temperatura maxima" : clima_des ['main']['temp_max'] , "Humedad" : clima_des ['main']['humidity'] , "Velocidad del viento" : clima_des ['wind']['speed'] , "Nubes" : clima_des ['clouds']['all']}
-
+        
     elif ciudad_org_cache and not ciudad_des_cache:
 
         clima_org = diccionario_guardar["Ciudad"].get(lista_coordenadas[int(indice_prop) - 1][0:3])
