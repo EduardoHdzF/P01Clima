1a7a907d9ff86d9d8ceb14ca6e90b6a31eb3da30
EduardoHdzF <eduardohf@ciencias.unam.mx>
2022-09-17 13:20:51 -0500

    Separación del código y creación de la clase API

--- ClimaVuelos/Programa/Entrada/datosEntrada.py
+++ ClimaVuelos/Programa/Entrada/datosEntrada.py
@@ -19,7 +19,41 @@ with open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+') as ArchivoEntrada:
     #print(ArchivoEntrada.readlines())
     print(Lista)
 '''
-ciudades_archivo_csv = open("dataset1.csv","r")
+
+ciudades_archivo_csv = open('ClimaVuelos\Programa\Entrada\dataset1.csv', 'r+')#"dataset1.csv","r")
 
 listaCoordenadas = []
 
+# Agregar las ciudades a mi lista (sin repeticiones).
+for iter in range(len(ciudades_archivo_csv.readlines())):
+    
+    if iter == 1:
+        ciudades_archivo_csv.seek(0)
+        ciudades_archivo_csv.readline()
+        listaCoordenadas.append(ciudades_archivo_csv.readline())
+        
+    elif iter > 1:                    
+        ciudades_archivo_csv.seek(0)
+
+        if not listaCoordenadas.count(ciudades_archivo_csv.readlines()[iter]):                                                
+            ciudades_archivo_csv.seek(0)
+            listaCoordenadas.append(ciudades_archivo_csv.readlines()[iter])                                
+            ciudades_archivo_csv.seek(0)
+
+listaCoordenadas.sort()
+
+print(listaCoordenadas)
+
+ # Creacion de una lista para el usuario para que pueda ver cual opcion escoger.
+lista_ciudades = []
+
+for iter in range(len(listaCoordenadas)):
+    
+    ciudades_org_des = [listaCoordenadas[iter][0:3] , listaCoordenadas[iter][4:7]]
+    lista_ciudades.append(ciudades_org_des)
+
+#posicion = 1
+for viaje in range(len(lista_ciudades)):
+
+    ciudad = 0
+    print("Vuelo número " + str(viaje+1) + ".- " , lista_ciudades[viaje][ciudad] , " -> " , lista_ciudades[viaje][ciudad + 1])
--- ClimaVuelos/Programa/principal.py
+++ ClimaVuelos/Programa/principal.py
@@ -1,7 +1,81 @@
 import Entrada.datosEntrada as entrada
+from Solicitud.SolicitaAPI import SolicitaApi 
 
+'''
 print("w")
 
 Hola = "perro"
 print(Hola[0:2])
 print("frencisco")
+'''
+def finaliza():
+    
+    verif = False
+
+    while not verif:
+
+        try:
+
+            termina = int(input("** Para regresar al menu de opciones presiona 1.\n** Para finalizar programa presiona 0.\n"))
+
+            if termina == 0 or termina == 1:
+
+                verif = True   
+
+            else:
+
+                print("ERROR : Solo se puede introducir valores 0 o 1")
+                verif = False             
+
+        except ValueError:
+            print("ERROR : Solo se pueden introducir valores numéricos.")
+
+    return termina
+
+print("w")
+
+terminado  = 1
+while terminado == 1:
+    print("--- CLIMA ---")
+    
+     # Para manejar las excepciones.
+    '''
+        La función manejor de errores nos ayuda a que cuando se elija un viaje, este exista dentro de la lista de
+        disponibles, y si no, que nos desplegue un mensaje de que no eligió un valor aceptable.
+        @param el número de ciudades que se tienen disponibles.
+
+    '''
+    def manejo_errores(canti_ciudades):
+        
+        verif = False
+
+        while not verif:
+
+            try:
+
+                ciudad_selec = int(input("Escoge un viaje:"))
+
+                if ciudad_selec > 0 and ciudad_selec <= canti_ciudades:
+
+                    verif = True   
+
+                else:
+
+                    print("ERROR : Solo se puede introducir valores entre 1 - " + str(canti_ciudades) + ".")
+                    verif = False             
+
+            except ValueError:
+                print("ERROR : Solo se pueden introducir valores numéricos.")
+
+        return ciudad_selec
+
+    indice_prop = manejo_errores(len(entrada.lista_ciudades))
+
+    # El usuario decide si se finaliza el programa o no.
+    terminado = finaliza()
+    coordenadas = entrada.listaCoordenadas
+    Clase = SolicitaApi(coordenadas ,indice_prop)
+    Clase.solicitarAPI()
+    diccionarioCoordenadas = Clase.identificarCoordenadas()
+    print(diccionarioCoordenadas)
+    entrada.ciudades_archivo_csv.close()
